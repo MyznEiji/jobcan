717c818a18f713168e2c43699d0838e37f493f50
MyznEiji <em711th@icloud.com>
2018-03-28 00:53:21 +0900

    hoge

--- seleniumTest.py
+++ seleniumTest.py
@@ -12,9 +12,6 @@ driver.get("https://www.google.co.jp/?gfe_rd=cr&dcr=0&ei=le65WqrLFaquX9-pqMAI")
 driver.find_element_by_name('q').send_keys("text")
 driver.find_element_by_name('btnK').click()
 
-webbrowser.open("https://master.tech-camp.in/admin/chat_messages")
-webbrowser.open("https://calendar.google.com/calendar/r")
-
 
 driver.close()
 driver.quit()
